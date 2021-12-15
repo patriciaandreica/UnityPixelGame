747c797249b59c12f689dca2f17b651a8fbc104e
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2021-12-15 16:56:55 -0500

    Added comments

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is another item derived from base class Item
+/// </summary>
+
 public class Weapon : Item
 {
-    [SerializeField] int damageAmount;
+    [SerializeField] private int damageAmount;
 
     public Weapon(int damage)
     {
