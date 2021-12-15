78c1d6f938eb73045e92644678247876d827bf07
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2021-12-15 16:55:45 -0500

    Added comments

--- Assets/Scripts/Items/Potion.cs
+++ Assets/Scripts/Items/Potion.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is used as one of the items dervied from the base class Items
+/// </summary>
+
 public class Potion : Item
 {
-    [SerializeField] private string colorPotion;
+    [SerializeField] private string colorPotion { get; set; }
 
-    public Potion(string color)
+    public Potion(string color) // constructor
     {
         this.colorPotion = color;
     }
+ 
 }
