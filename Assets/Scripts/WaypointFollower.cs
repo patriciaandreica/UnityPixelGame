705bf1e140636482b12a629f95de4507da70f3e0
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2021-12-15 16:53:42 -0500

    Added comments

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -2,24 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The purpose of this class is to use waypoints. The purpose of waypoints
+/// is to allow your object to move from one point to another and repeat. You can have multiple waypoints
+/// but I only used two throughout the game, so that an object can move back and forth.
+/// </summary>
+
 public class WaypointFollower : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] waypoints; // creating an array to have multiple waypoints if wanted
     private int currentWaypointIndex = 0; // first index
 
-    [SerializeField] private float speed = 2f; // speed of platform
+    [SerializeField] private float platformSpeed = 2f; // speed of platform
+
   void Update()
-    {
-      if(Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f) // calculate distance between vectors (waypoint) and if close to 0
+{
+        Flip();
+}
+
+ /// <summary>
+ /// The purpose of this method is so that the object will walk to one point 
+ /// and once reached, it will flip and keep repeating by resetting the index to zero. 
+ /// </summary>
+private void Flip()
+{
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f) // calculate distance between vectors (waypoint) and if close to 0
         {
             currentWaypointIndex++; // switch to next waypoint (object goes other way)
-            if(currentWaypointIndex >= waypoints.Length) // if at last waypoint
+            if (currentWaypointIndex >= waypoints.Length) // if at last waypoint
             {
                 currentWaypointIndex = 0; // reset again to 0 so it repeats
             }
         }
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed); // move platform every frame
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * platformSpeed); // move platform every frame
     }
 }
 
