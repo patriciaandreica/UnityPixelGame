11dfb4e3d01159c4856d56ed59421d9a78f54424
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2021-12-15 15:09:02 -0500

    Update Enemy.cs
    Added comments

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,11 +7,18 @@ public class Enemy : PlayerHealth
 {
     private int damageAmount;
     protected float enemySpeed = 1f;
+
+    /// <summary>
+    /// Enemy object movemement
+    /// </summary>
     protected virtual void EnemyMovement() // Virtual method can be overriden
     {
         transform.Rotate(0, 360 * enemySpeed * Time.deltaTime, 0); // Rotate on y-axis
     }
-
+    /// <summary>
+    /// If player collides with enemy, then reload scene
+    /// </summary>
+    /// <param name="collision"></param>
     protected override void OnCollisionEnter2D(Collision2D collision) // Overriding so when player collides with enemy the player dies instantly
     {
         if (collision.gameObject.tag == "Player") 
@@ -20,6 +27,9 @@ public class Enemy : PlayerHealth
         }
     }
 
+    /// <summary>
+    /// Enemy introduction - this will just appear in the console 
+    /// </summary>
     public virtual void Introduction()
     {
         Debug.Log("Enemy!");
