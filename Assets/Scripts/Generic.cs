8362f214456cfa6de403ab1d08ef95abe7edc9dc
patriciaandreica <68759170+patriciaandreica@users.noreply.github.com>
2021-12-15 16:42:46 -0500

    Added comments

--- Assets/Scripts/Generic.cs
+++ Assets/Scripts/Generic.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// Generic type
+/// <summary>
+/// This class is a generic type for items to create a list
+/// </summary>
 public class Generic : MonoBehaviour
 {
     [SerializeField] private List<Item> items = new List<Item>(); // List of type Item
 
     private void Start()
     {
-        items.Add(new Potion("Red")); 
+        items.Add(new Potion("Red"));  // pass to constructor and add red poition
         items.Add(new Weapon(50));
         items.Add(new Potion("Blue"));
         items.Add(new Weapon(30));
@@ -17,16 +19,21 @@ public class Generic : MonoBehaviour
         List<Item> listPotions = GetItemsType<Potion>(); // Potion can also be passed
 
     }
+    /// <summary>
+    /// This is a generic method used for creating a list based on type T
+    /// </summary>
+    /// <typeparam name="T">Any type of item </typeparam>
+    /// <returns>A list of items depending on type</returns>
     private List<Item> GetItemsType<T>() where T : Item   // Take any type derived from Item
     {
-        List<Item> itemsToReturn = new List<Item>(); 
+        List<Item> returnItems = new List<Item>(); 
         for(int i = 0; i < items.Count; i++) // Loop through items
         {
             if(items[i] is T) // Current item is type T
             {
-                itemsToReturn.Add(items[i]); // Add item to list
+                returnItems.Add(items[i]); // Add item to list
             }
         }
-        return itemsToReturn;
+        return returnItems;
     }
 }
